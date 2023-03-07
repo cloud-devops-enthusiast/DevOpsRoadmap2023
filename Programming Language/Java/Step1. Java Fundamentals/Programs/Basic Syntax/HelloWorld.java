449b7583d788bba1eebe7acd7425b19c16a3fe4f
Abhinav Singh <abhinav41999@gmail.com>
2023-03-08 01:18:24 +0530

    Create HelloWorld.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Basic Syntax/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+    public static void main(String[] args)
+    // This will print "Hello World!" on the screen
+    {
+        System.out.println("Hello World!");
+        //Output: Hello World!
+    }
+}
