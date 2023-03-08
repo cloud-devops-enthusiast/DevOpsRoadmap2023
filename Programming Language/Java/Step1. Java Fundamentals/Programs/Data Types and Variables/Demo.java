3e511fd3e717a76def927293c2a178044b25e608
Abhinav Singh <abhinav41999@gmail.com>
2023-03-08 23:10:01 +0530

    Create Demo.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Data Types and Variables/Demo.java
@@ -0,0 +1,26 @@
+class Demo{
+    public static void main(String[] args){
+        byte x;
+        int a = 270;
+        double b = 128.128;
+        System.out.println("Int coverted to byte.");
+        x = (byte) a;
+        System.out.println("a and x:" + a + "" + x);
+        System.out.println("Double coverted to int.");
+        a = (int) b;
+        System.out.println("b and a:" + b + "" + a);
+        System.out.println("\nDouble coverted to byte.");
+        x = (byte) b;
+        System.out.println("b and x:" + b + "" + x);
+    }
+}
+
+/*Output:
+Int coverted to byte.
+a and x:27014
+Double coverted to int.
+b and a:128.128128      
+
+Double coverted to byte.
+b and x:128.128-128
+*/
