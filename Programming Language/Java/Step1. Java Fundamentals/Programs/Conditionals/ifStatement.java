e2e1158744cd92bc9a7cf281f7526d6778c7bd29
Abhinav Singh <abhinav41999@gmail.com>
2023-03-11 00:48:14 +0530

    Create ifStatement.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Conditionals/ifStatement.java
@@ -0,0 +1,13 @@
+public class ifStatement {
+    public static void main(String[] args){
+        int age;
+        age = 23;
+        if (age > 18){
+            System.out.println("Your age is greater than 18");
+        }
+    }
+}
+/*
+Output:
+Your age is greater than 18
+ */
