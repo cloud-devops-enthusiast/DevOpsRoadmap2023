b1f59d168f7888bcc2bd37eda7620e9c30df41eb
Abhinav Singh <abhinav41999@gmail.com>
2023-03-10 22:16:15 +0530

    Create addingLowerType.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Data Types and Variables/addingLowerType.java
@@ -0,0 +1,12 @@
+public class addingLowerType {
+    public static void main(String[] args){
+        byte a = 10;
+        byte b = 20;
+        byte c = (byte) (a + b);
+        System.out.println("Sum of a and b is: "+c);
+    }
+}
+/*
+Output:
+Sum of a and b is: 30
+ */
