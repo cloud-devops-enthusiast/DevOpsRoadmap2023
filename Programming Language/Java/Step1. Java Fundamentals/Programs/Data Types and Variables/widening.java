4ee563d758b480bca1877e42bfc92d9de013d7d9
Abhinav Singh <abhinav41999@gmail.com>
2023-03-10 22:02:21 +0530

    Create widening.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Data Types and Variables/widening.java
@@ -0,0 +1,14 @@
+public class widening {
+    public static void main(String[] args){
+        int a = 10;
+        float b = a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
+/*
+Output:
+a = 10
+b = 10.0
+*/
