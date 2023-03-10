535dc8d0e854d5fc7e6ab2ed09d0eb82de153e0a
Abhinav Singh <abhinav41999@gmail.com>
2023-03-10 22:02:24 +0530

    Create addition.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Data Types and Variables/addition.java
@@ -0,0 +1,14 @@
+public class addition{
+    public static void main(String[] args){
+        int a;
+        a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("Sum of a and b is: "+c);
+    }
+}
+
+/*
+Output:
+Sum of a and b is: 30
+ */
