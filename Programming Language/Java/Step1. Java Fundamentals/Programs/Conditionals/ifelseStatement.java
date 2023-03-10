da8e2e3f2b8287a2ac5bbbd1a256ea8f7cbbe1f6
Abhinav Singh <abhinav41999@gmail.com>
2023-03-11 00:48:10 +0530

    Create ifelseStatement.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Conditionals/ifelseStatement.java
@@ -0,0 +1,16 @@
+public class ifelseStatement {
+    public static void main(String[] args){
+        int number;
+        number=23;
+        if (number%2==0){
+            System.out.println(number+" is an even number");
+        }
+        else{
+            System.out.println(number+" is an odd number");
+        }
+    }
+}
+/*
+Output:
+23 is an odd number
+ */
