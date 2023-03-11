13971444b5aedc0668297533e4494dc0197bfdc3
Abhinav Singh <abhinav41999@gmail.com>
2023-03-11 23:16:25 +0530

    Create ternaryOperator.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Conditionals/ternaryOperator.java
@@ -0,0 +1,12 @@
+public class ternaryOperator{
+    public static void main(String[] args){
+        int number = 10;
+        //Using ternary operator
+        String Output=(number%2==0)?"Even Number":"Odd Number";
+        System.out.println(Output);
+    }
+}
+/*
+Output:
+Even Number
+ */
