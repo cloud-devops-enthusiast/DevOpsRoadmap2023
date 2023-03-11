88e80f7a2f7cfda89d5b0ef907a1b44b73472c95
Abhinav Singh <abhinav41999@gmail.com>
2023-03-12 01:12:54 +0530

    Create predefinedMethod.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Functions/predefinedMethod.java
@@ -0,0 +1,9 @@
+public class predefinedMethod{
+    public static void main(String[] args){
+        System.out.println("The maximum of 3 and 4 is " + Math.max(3, 4));
+    }
+}
+/*
+Output:
+The maximum of 3 and 4 is 4
+*/
