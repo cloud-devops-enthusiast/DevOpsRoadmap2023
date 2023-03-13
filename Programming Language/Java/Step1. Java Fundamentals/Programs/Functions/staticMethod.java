b8f93bd66aedb6fa7af871a946d337a842e4c6e0
Abhinav Singh <abhinav41999@gmail.com>
2023-03-13 22:03:37 +0530

    Create staticMethod.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Functions/staticMethod.java
@@ -0,0 +1,12 @@
+public class staticMethod {
+    public static void main(String[] args){
+        show();
+    }
+    static void show(){
+        System.out.println("This is a static method");
+    }
+}
+/*
+Output:
+This is a static method
+*/
