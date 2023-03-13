181ba2e7abd64735ed5f56a90f0e3505ce519051
Abhinav Singh <abhinav41999@gmail.com>
2023-03-14 00:32:29 +0530

    Create simpleForLoop.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Loops/simpleForLoop.java
@@ -0,0 +1,21 @@
+public class simpleForLoop {
+    public static void main(String[] args) {  
+        //Code of Java for loop  
+        for(int i=1;i<=10;i++){  
+            System.out.println(i);  
+        }  
+    }  
+}
+/*
+Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
