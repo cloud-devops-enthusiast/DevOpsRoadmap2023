543653de004a95d0e186eee541218ac8a08836b4
Abhinav Singh <abhinav41999@gmail.com>
2023-03-14 00:32:25 +0530

    Create myMethod.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Functions/myMethod.java
@@ -0,0 +1,16 @@
+public class myMethod {
+    static void myMethod() {
+        System.out.println("My name is abhinav! I am a knucklehead X:");
+    }
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();        
+    }
+}
+/*
+Output:
+My name is abhinav! I am a knucklehead X:
+My name is abhinav! I am a knucklehead X:
+My name is abhinav! I am a knucklehead X:
+*/
