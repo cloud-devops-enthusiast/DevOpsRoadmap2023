7136fccce7027c7357a99b93dc506aab43886ffe
Abhinav Singh <abhinav41999@gmail.com>
2023-03-14 00:32:22 +0530

    Create checkAge.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Functions/checkAge.java
@@ -0,0 +1,16 @@
+public class checkAge {
+    static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println("Access denied - You are not old enough!");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+    public static void main(String[] args) {
+        checkAge(20);
+    }
+}
+/*
+Output:
+Access granted - You are old enough!
+*/
