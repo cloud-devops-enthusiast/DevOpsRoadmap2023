9285aca1a3890c82eecbcb86de74c1cf326d26b5
Abhinav Singh <abhinav41999@gmail.com>
2023-03-15 00:12:12 +0530

    Create forEachLoop.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Loops/forEachLoop.java
@@ -0,0 +1,22 @@
+public class forEachLoop {
+    public static void main(String[] args){
+        int arr[] = {95,61,62,24,49,84,59,49,69,62};
+        for(int i:arr){
+            System.out.println(i);
+        }
+      System.out.println(""); //For new line
+    }
+}
+/*
+Output:
+95
+61
+62
+24
+49
+84
+59
+49
+69
+62
+*/
