d604f213fcac61641012f140ea847196343efbdc
Abhinav Singh <abhinav41999@gmail.com>
2023-03-15 00:12:43 +0530

    Create simpleWhileLoop.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Loops/simpleWhileLoop.java
@@ -0,0 +1,22 @@
+public class simpleWhileLoop {
+    public static void main(String[] args){
+        int i = 0;
+        while(i<10){
+            System.out.println(i);
+        i++;
+        }
+    }
+}
+/*
+Output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+*/
