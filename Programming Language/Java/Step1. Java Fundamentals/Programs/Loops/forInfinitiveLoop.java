0e2589c09a2cf85c82ddd8cae9e81dd00ef0bc35
Abhinav Singh <abhinav41999@gmail.com>
2023-03-15 00:12:15 +0530

    Create forInfinitiveLoop.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Loops/forInfinitiveLoop.java
@@ -0,0 +1,29 @@
+public class forInfinitiveLoop {
+    public static void main(String[] args){
+        for(;;){
+            System.out.println("infinitive loop");
+        }
+    }
+}
+/*
+Output:
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+infinitive loop
+*/
