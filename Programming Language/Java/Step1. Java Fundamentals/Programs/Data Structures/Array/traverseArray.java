555a8baf47a79ff7eee3392b0c821ce8c4f83b84
Abhinav Singh <abhinav41999@gmail.com>
2023-03-23 00:18:01 +0530

    Create traverseArray.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Data Structures/Array/traverseArray.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class traverseArray {
+    public static void main(String[] args){
+        int arr[] = {95,61,62,24,49};
+        int i;
+        System.out.println("Elements of array are:");
+        for(i=0;i<arr.length;i++){
+            System.out.println(arr[i]+"[Index:"+i+"]");
+        }
+    }
+}
+/*
+Output:
+Elements of array are:
+95[Index:0]
+61[Index:1]
+62[Index:2]
+24[Index:3]
+49[Index:4]
+*/
