a839b35ccdc38f1fe2ef408ef23ad3ee981caff4
Abhinav Singh <abhinav41999@gmail.com>
2023-03-22 22:36:14 +0530

    Create accessArray.java

---
+++ Programming Language/Java/Step1. Java Fundamentals/Programs/Data Structures/Array/accessArray.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class accessArray {
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        //Declaring a new integer array and assigning values to it
+        System.out.println(arr[4]);
+        //Print the value at index 4
+    }
+}
+/*
+Output:
+5
+*/
